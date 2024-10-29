9176da4a85b50356c71094bb8a7674278e43857c
Saalim Aqueel <aqueel192@gmail.com>
2024-10-29 21:57:03 +0530

    logged Events + used chalk+figlet for colors and ASCII art

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import figlet from "figlet";
 dotenv.config({ path: "./" });
 
 const TOKEN = process.env.GITHUB_TOKEN;
-
 const user = "SxxAq";
 const url = `https://api.github.com/users/${user}/events`;
 
@@ -18,9 +17,83 @@ const fetchUserEvent = async () => {
       },
     });
     const activity = response.data;
-    console.log(activity);
+    logUserActivity(activity);
   } catch (error) {
-    console.log("Couldnt fetch user Activity\n", error);
+    console.log(
+      chalk.red("Couldnt fetch user Activity\n"),
+      chalk.yellow(error)
+    );
   }
 };
+
+const logUserActivity = (activity) => {
+  console.log(
+    chalk.blue(
+      figlet.textSync("Github Activity", {
+        font: "Standard",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+      })
+    )
+  );
+  activity.forEach((event) => {
+    const { type, repo, payload } = event;
+
+    switch (type) {
+      case "PushEvent":
+        console.log(
+          chalk.green(
+            `Pushed ${payload.commits.length} commit(s) to ${repo.name}`
+          )
+        );
+        break;
+      case "IssuesEvent":
+        if (payload.action === "opened") {
+          console.log(
+            chalk.blue(
+              `Opened a new issue in ${repo.name}: ${payload.issue.title}`
+            )
+          );
+        } else if (payload.action === "closed") {
+          console.log(
+            chalk.blue(
+              `Closed an issue in ${repo.name}: ${payload.issue.title}`
+            )
+          );
+        }
+        break;
+      case "WatchEvent":
+        if (payload.action === "started") {
+          console.log(chalk.magenta(`Starred ${repo.name}`));
+        }
+        break;
+      case "ForkEvent":
+        console.log(chalk.green(`Forked ${repo.name}`));
+        break;
+      case "CreateEvent":
+        if (payload.ref_type === "repository") {
+          console.log(chalk.green(`Created repository ${repo.name}`));
+        } else if (payload.ref_type === "branch") {
+          console.log(
+            chalk.green(`Created branch ${payload.ref} in ${repo.name}`)
+          );
+        }
+        break;
+      case "DeleteEvent":
+        if (payload.ref_type === "branch") {
+          console.log(
+            chalk.red(`Deleted branch ${payload.ref} in ${repo.name}`)
+          );
+        } else if (payload.ref_type === "tag") {
+          console.log(chalk.red(`Deleted tag ${payload.ref} in ${repo.name}`));
+        }
+        break;
+      // Add more cases as needed
+      default:
+        console.log(chalk.gray(`${type} in ${repo.name}`));
+    }
+    // console.log(chalk.green(`- ${event.type}: ${event.repo.name}`));
+  });
+};
+
 fetchUserEvent();
